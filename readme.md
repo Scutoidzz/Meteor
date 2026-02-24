c06834c4ca0b7aeea2dabbb79dd8b7f5eea62a8c
Sterling Duncan <techedreal@gmail.com>
2026-02-24 09:30:54 -0600

    gofot ahh refactor

--- readme.md
+++
@@ -1,7 +0,0 @@
-# Meteor 
-###  A self hosted media server
-
-
-
-### Things to know
-+ During the setup, you will have to keep a window open until it is complete. You won't need to do this after the first time.
---
+++ server/controlpanel.py
@@ -0,0 +1,8 @@
+import PyQt6.QtWidgets as QtWidgets
+import PyQt6.QtCore as QtCore
+import PyQt6.QtGui as QtGui
+
+class ControlPanel():
+    def __init__(self):
+        self.window = QtWidgets.QtWidget()
+        
